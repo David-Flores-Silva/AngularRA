c08aa1de1a1b2d4d6450b41fb0f5b647e5320c49
David Flores Silva <dfloressi@unsa.edu.pe>
2021-08-09 11:59:57 -0500

    sgregando un nuevo tipo a models

--- proyectoAngular/jdangular/api/models.py
+++ proyectoAngular/jdangular/api/models.py
@@ -6,7 +6,7 @@ class Movie(models.Model):
     title = models.CharField(max_length=32)
     desc = models.CharField(max_length=256)
     year = models.IntegerField()
-    img = models.ImageField(upload_to="pics")
+    img = models.ImageField()
 
     def get_absolute_url(self):
         return Reversible("movies:Movie_Detail", kwargs={"pk": self.id})
